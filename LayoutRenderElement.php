278b2e804030fb39e7cf545164c86382d27fa10a
DominicWatts <dominic.watts@hotmail.co.uk>
2019-10-23 22:46:59 +0100

    Create LayoutRenderElement.php

---
+++ LayoutRenderElement.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Xigen\DeliveryComment\Observer\Frontend\Core;
+
+use Magento\Framework\Event\Observer as EventObserver;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\View\Element\TemplateFactory;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class LayoutRenderElement
+ * @package Xigen\DeliveryComment\Observer\Frontend\Core
+ */
+class LayoutRenderElement implements \Magento\Framework\Event\ObserverInterface
+{
+    /**
+     * @var TemplateFactory
+     */
+    protected $templateFactory;
+
+    /**
+     * LayoutRenderElement constructor.
+     * @param TemplateFactory $templateFactory
+     */
+    public function __construct(
+        TemplateFactory $templateFactory
+    ) {
+        $this->templateFactory = $templateFactory;
+    }
+
+    /**
+     * Execute observer
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(EventObserver $observer)
+    {
+        if ($observer->getElementName() == 'sales.order.info') {
+            $orderShippingViewBlock = $observer->getLayout()->getBlock($observer->getElementName());
+            $order = $orderShippingViewBlock->getOrder();
+            $deliveryCommentBlock = $this->templateFactory->create();
+            $deliveryCommentBlock->setDeliveryComment($order->getDeliveryComment());
+            $deliveryCommentBlock->setTemplate('Xigen_DeliveryComment::order_info_shipping_info.phtml');
+            $html = $observer->getTransport()->getOutput() . $deliveryCommentBlock->toHtml();
+            $observer->getTransport()->setOutput($html);
+        }
+        return $this;
+    }
+}
